1986136dd15fba95e6b4d80254a8a0b56c29ae01
Haseebyaseen08 <haseeb.yaseen08@gmail.com>
2020-12-22 12:03:54 +0500

    Create items.js

---
+++ React JS/project/src/Pages/items.js
@@ -0,0 +1,39 @@
+export default {
+    products:[
+        {
+          name:"Funky Hoodie",
+          youtuber:"Awsamo Speakes",
+          image:"./content.jpg",
+          price:"PKR 1065",
+          Status:"Available"
+        },
+        {
+          name:"Funky Hoodie",
+          youtuber:"Mooro",
+          image:"./content.jpg",
+          price:"PKR 1165",
+          Status:"Sold Out"
+        },
+        {
+          name:"Funky Hoodie",
+          youtuber:"RHS",
+          image:"./content.jpg",
+          price:"PKR 965",
+          Status:"Available"
+        },
+        {
+          name:"Funky Hoodie",
+          youtuber:"Ducky Bhai",
+          image:"./content.jpg",
+          price:"PKR 1065",
+          Status:"Available"
+        },
+        {
+            name:"Funky Hoodie",
+            youtuber:"The Idiotz",
+            image:"./content.jpg",
+            price:"PKR 1365",
+            Status:"Sold Out"
+        }
+      ]
+}
